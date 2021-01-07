a67035786b4c64e14e7b8e2eb64b495667a21d2f
hoogenb2 <70774522+hoogenb2@users.noreply.github.com>
2021-01-07 16:12:17 -0500

    solution to test cases 1 and 2

--- solution.py
+++ solution.py
@@ -1,3 +1,42 @@
 def solution(floor):
-    # your code goes here
-    return None
+    """
+    >>> floor1 = [ [0, 1], [0, 0] ]
+    >>> solution(floor1)
+    3
+    >>> floor2 = [ [0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0] ]
+    >>> solution(floor2)
+    7
+    """
+    j = 0
+    i = 0
+    count = 1
+
+    while (i < len(floor)) and (j < len(floor[i])):  # height and width
+        if (i + 1 < len(floor)) and floor[i + 1][j] == 0:
+            i += 1
+            count += 1
+        elif (j + 1 < len(floor[i])) and floor[i][j + 1] == 0:
+            j += 1
+            count += 1
+        elif (i + 1 == len(floor)) and (j + 1 == len(floor[i])):
+            break
+        elif (i - 1 >= 0) and floor[i-1][j]:
+            i -= 1
+            count += 1
+        elif (j - 1 >= 0) and floor[i][j - 1]:
+            j -= 1
+            count += 1
+
+    return count
+
+    # repeat below until at bottom right most corner
+        # is the spot to the right a wall or floor
+        # is the spot to the bottom a wall or floor
+        # is the spot to the left a wall or floor
+        # is the spot to the top a wall or floor
+        # physically move and increment count once i know it is a floor
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
